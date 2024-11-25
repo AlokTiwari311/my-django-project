e3c0e011740f1295cea44ae7abfd1ee28a7b2b78
AlokTiwari311 <AlokTiwari311113@gmail.com>
2024-11-26 00:04:55 +0530

    Initial commit

--- campaign_project/settings.py
+++ campaign_project/settings.py
@@ -128,6 +128,10 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Use Whitenoise to manage static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
